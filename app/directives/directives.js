cabeac3e7bdd7cbd5d8224d0abcec36532743e9e
mikeg <mike@growth-x.com>
2019-03-26 13:35:34 +0200

    added spellchecker and electron-rebuild

--- app/directives/directives.js
+++
@@ -1,8 +0,0 @@
-// var app = angular.module('app.directives', []);
-//
-// app.directive('startPage', function () {
-//     return{
-//         restrict: 'E',
-//         templateUrl: 'templates/startPageTemplate.html'
-//     }
-// })
